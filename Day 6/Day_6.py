9f7b48693aa21a61c0165e5b49a357f27103c329
Agustín Fernández <anf90.e@gmail.comm>
2022-07-18 23:12:27 -0300

    Day 6, functions

---
+++ Day 6/Day_6.py
@@ -0,0 +1,56 @@
+#Hurdle 4
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+# def jump():
+#     while (wall_in_front()):
+#         turn_left()
+#         move()
+#         turn_right()  
+#     move()
+#     turn_right()
+#     while (front_is_clear()): 
+#         move()
+#     turn_left()
+    
+# while not at_goal():
+#     if (not wall_in_front()):
+#         move()
+#     elif (wall_in_front()):
+#         jump()
+
+
+# Final Project - Escaping the Maze
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+    
+# while not at_goal():
+#     if (not wall_on_right()):
+#         turn_right()
+#         move()
+#     elif (front_is_clear()):
+#         move()
+#     else:
+#         turn_left()
+
+
+# Reeborg World Tests
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+    
+# while not at_goal():
+#     if ((not wall_on_right()) and front_is_clear()):
+#         move()
+#     elif (not wall_on_right()):
+#         turn_right()
+#         move()
+#     elif (front_is_clear()):
+#         move()
+#     else:
+#         turn_left()
